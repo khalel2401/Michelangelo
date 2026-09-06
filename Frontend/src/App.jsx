b6d9a6068bf86d38021ad7fbe5295c286bdca682
Simon Reyes <simon.reyes2401@alumnos.ubiobio.cl>
2026-09-06 17:57:30 -0300

    Feat(FrontEnd): Creado el elemento TopTabs.jsx

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -3,19 +3,27 @@ import Container from '@mui/material/Container';
 import Typography from '@mui/material/Typography';
 import Box from '@mui/material/Box';
 import ProTip from './ProTip';
+import TopTabs from './topTabs';
 
 
 export default function App() {
+
   return (
     <Container maxWidth="sm">
       <Box sx={{ my: 4 }}>
         
+        {/* asi es como se pone un texto random  */}
         <Typography variant="h4" component="h1" gutterBottom>
           HOLA MUNDO
         </Typography>
+        
+        {/* Aca esta la sección de tabs */}
+        <TopTabs />
+        
         {/* para añadir una parte a la app usar <"componente"/>   */}
         <ProTip />
 
+        
       </Box>
     </Container>
   );
